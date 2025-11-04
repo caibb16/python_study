529ca153532fecf61b2e365f5e21f4fe8e589011
caibb16 <2820875472@qq.com>
2025-11-04 15:28:03 +0800

    新增 EncoderRNN 类及其前向传播方法

---
+++ 大模型/Seq2Seq/encoderRNN.py
@@ -0,0 +1,28 @@
+import torch as th
+import torch.nn as nn
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_dim, hidden_dim, dropout_p=0):
+        super(EncoderRNN, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.embedding = nn.Embedding(input_dim, hidden_dim) # 输入维度到隐藏维度的嵌入层
+        self.rnn = nn.RNN(hidden_dim, hidden_dim, dropout=dropout_p, batch_first=True) # RNN层
+        self.dropout = nn.Dropout(dropout_p)
+
+    def forward(self, x):
+        x = self.embedding(x)
+        x = self.dropout(x)
+        output, hidden = self.rnn(x)
+        return output, hidden
+    
+#test
+if __name__ == "__main__":
+    encoder = EncoderRNN(input_dim=10, hidden_dim=5)  # 词表大小为10，隐藏层大小为5
+    input_vector = th.tensor([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]])
+    embedded = encoder.embedding(input_vector) # 获取嵌入向量,嵌入层将输入序列的每个元素映射到一个隐藏维度的向量
+    output, hidden = encoder(input_vector)
+    print("输入序列的维度：", input_vector.shape)
+    print("输入序列经过嵌入层后的维度:", embedded.shape)
+    print("输出向量的维度:", output.shape)  #output.shape = (batch_size, seq_len, hidden_dim)
+    print("隐藏状态的维度:", hidden.shape)  #hidden.shape = (num_layers, batch_size, hidden_dim)
