7ffa1003a68f74e1ef5ee357cc9d9862cce8ffba
caibb16 <2820875472@qq.com>
2025-11-05 17:32:58 +0800

    新增测试脚本以验证注意力解码器RNN及其他RNN模块

---
+++ 大模型/Seq2Seq/test.py
@@ -0,0 +1,9 @@
+from AttentionDecoderRNN import test_attention_decoder_rnn
+from EncoderRNN import test_encoder_rnn
+from DecoderRNN import test_decoder_rnn
+
+if __name__ == "__main__":
+
+    test_attention_decoder_rnn()
+    #test_encoder_rnn()
+    #test_decoder_rnn()
