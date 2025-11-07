3843afc3c7d1c67b03686415bf667443bdfec2ff
caibb16 <2820875472@qq.com>
2025-11-07 17:47:43 +0800

    新增注释

--- 大模型/Seq2Seq/test.py
+++ 大模型/Seq2Seq/test.py
@@ -1,7 +1,9 @@
 from AttentionDecoderRNN import test_attention_decoder_rnn
 from EncoderRNN import test_encoder_rnn
 from DecoderRNN import test_decoder_rnn
-
+'''
+测试编码器和解码器的输入和输出的形状
+'''
 if __name__ == "__main__":
 
     test_attention_decoder_rnn()
